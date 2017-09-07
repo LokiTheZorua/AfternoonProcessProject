c782f47655a4b731db4b2322bcf2ef89f86104c4
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-07 12:23:28 -0600

    Made it run

--- src/prossess/controller/ProcessController.java
+++ src/prossess/controller/ProcessController.java
@@ -2,5 +2,9 @@ package prossess.controller;
 
 public class ProcessController
 {
-
+	public void start()
+	{
+		System.out.println("This is a practice project");
+		System.out.println("By practiving I develp muscle memory");
+	}
 }
--- src/prossess/controller/ProsessRunner.java
+++ src/prossess/controller/ProsessRunner.java
@@ -4,6 +4,7 @@ public class ProsessRunner
 {
 	public static void main(String[] args)
 	{
-		
+		ProcessController appController = new ProcessController();
+		appController.start(); 
 	}
 }
