94d3944036b54c04eb1ec9a92552cf2d5dcad9cc
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-11 12:22:01 -0600

    added a second generator

--- src/prosses/model/PlayDohScircle.java
+++ src/prosses/model/PlayDohScircle.java
@@ -2,5 +2,18 @@ package prosses.model;
 
 public class PlayDohScircle
 {
-
+	//Decloration Section
+	//Data member section
+	private int size;
+	
+		public PlayDohScircle()
+		{
+			this.size = 5;
+		}
+		
+		public String toString()
+		{
+			String description = "I am a circle, my se is " + size;
+			return description;
+		}
 }
--- src/prossess/controller/ProcessController.java
+++ src/prossess/controller/ProcessController.java
@@ -1,10 +1,20 @@
 package prossess.controller;
 
+import prosses.model.PlayDohScircle;
+
 public class ProcessController
 {
 	public void start()
 	{
 		System.out.println("This is a practice project");
 		System.out.println("By practiving I develp muscle memory");
+		
+		PlayDohScircle firstCircle = new PlayDohScircle();
+		PlayDohScircle secondCircle;
+		secondCircle =new PlayDohScircle();
+		System.out.println("Look, I made a circle");
+		System.out.println(firstCircle);
+		System.out.println(secondCircle);
+		
 	}
 }
